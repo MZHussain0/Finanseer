5fc1f7244c252cdf556031999eefe05384b12201
mzhussain0 <mzhussain092@gmail.com>
2023-04-18 00:13:04 +0530

    mui theme and router configs

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,29 @@
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { useMemo } from "react";
+import { themeSettings } from "./theme";
+import { Box, CssBaseline } from "@mui/material";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 function App() {
-  return <div className="app">hello</div>;
+  const theme = useMemo(() => createTheme(themeSettings), []);
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+            <Routes>
+              <Route path="/" element={<div>Dashboard</div>} />
+              <Route
+                path="/predictions"
+                element={<div>Predictions page</div>}
+              />
+            </Routes>
+          </Box>
+        </ThemeProvider>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
